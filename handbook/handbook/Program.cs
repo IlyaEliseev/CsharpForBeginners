1b9552972b565e6d3bf4461f2c74e74a6762c4af
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2021-05-16 21:49:00 +0300

    handbook

---
+++ handbook/handbook/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace handbook
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte lengthMassives = 3;
+            string[] names = new string[lengthMassives];
+            string[] telephoneNambers = new string[lengthMassives];
+            byte[] listsAges = new byte[lengthMassives];
+
+            for(int i = 0; i < lengthMassives; i++)
+            {
+                Console.WriteLine("input name");
+                string name = Console.ReadLine();
+                names[i] = name;
+
+                Console.WriteLine("input telephone namber");
+                string telephoneNumber = Console.ReadLine();
+                telephoneNambers[i] = telephoneNumber;
+
+                Console.WriteLine("input age");
+                byte age = byte.Parse(Console.ReadLine());
+                
+            }
+
+            for (int i = 0; i < lengthMassives; i++)
+            {
+                Console.WriteLine($"name: {names[i]} telephone number: {telephoneNambers[i]} age: {listsAges[i]}");
+            }
+
+        }                                        
+    }
+}
