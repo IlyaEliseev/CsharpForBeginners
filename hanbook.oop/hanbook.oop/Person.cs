564c6b699636bb3f85166fe7e33dfedb52649f82
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2021-05-28 00:13:26 +0300

    handbook in oop style

---
+++ hanbook.oop/hanbook.oop/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hanbook.oop
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string TelephoneNumber { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string name, string telephoneNumber, byte age)
+        {
+            Name = name;
+            TelephoneNumber = telephoneNumber;
+            Age = age;
+        }
+
+        public void DisplayPersonInformation()
+        {
+            Console.WriteLine($"Name: {Name} Telephone Number: {TelephoneNumber} Age: {Age}");
+        }
+
+    }
+}
---
+++ hanbook.oop/hanbook.oop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hanbook.oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte lengthHandbook = 3;
+            
+            Person[] persons = new Person[lengthHandbook];
+
+            for (int i = 0; i < lengthHandbook; i++)
+            {
+                
+
+                Console.WriteLine("input name");
+                string _name = Console.ReadLine();
+                
+                Console.WriteLine("input telephone namber");
+                string _telephoneNumber = Console.ReadLine();
+                
+                Console.WriteLine("input age");
+                byte _age = byte.Parse(Console.ReadLine());
+
+                persons[i] = new Person(_name, _telephoneNumber, _age);
+                
+            }
+            
+            for (int i = 0; i < lengthHandbook; i++)
+            {
+                persons[i].DisplayPersonInformation();
+            }
+        }
+    }
+}
