163131f3d7c7bbf499e56cb43d67c6b3fa311958
IlyaEliseev <ilyaeliseevjob@gmail.com>
2021-09-16 17:26:13 +0300

    add class Messeges

---
+++ Shop/Shop/Messeges.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Messeges
+    {
+        public static void ErrorCountMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Empty!");
+            Console.ResetColor();
+        }
+
+        public static void OutOfRangeIndex()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Index not found!");
+            Console.ResetColor();
+        }
+
+        public static void DeliteShowcaseMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Showcase is not empty!");
+            Console.ResetColor();
+        }
+    }
+}
