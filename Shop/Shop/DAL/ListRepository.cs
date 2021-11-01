021c166ccf3df0258e25cddf15cab8e503e4a9ef
IlyaEliseev <ilyaeliseevjob@gmail.com>
2021-11-01 16:50:52 +0300

    #8 add class

---
+++ Shop/Shop/DAL/ListRepository.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Services;
+using Shop.Interfaces;
+
+namespace Shop.DAL
+{
+    public class ListRepository
+    {
+        private List<Product> _products;
+        public IProductService ProductService { get; private set; }
+
+        public ListRepository(IProductService productService)
+        {
+            _products = new List<Product>();
+            ProductService = productService;
+        }
+
+    }
+}
